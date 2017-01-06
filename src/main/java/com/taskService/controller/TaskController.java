41e7d0f814564f157aef13c3f99514ef5d8e55a6
shoiab khan <shoiabkhan32@gmail.com>
2017-01-06 14:16:00 +0530

    sendemail api changes

--- src/main/java/com/taskService/controller/TaskController.java
+++ src/main/java/com/taskService/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.taskService.controller;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
@@ -9,13 +10,17 @@ import org.apache.solr.common.SolrDocumentList;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.google.gson.Gson;
 import com.taskService.model.TaskModel;
 import com.taskService.service.data.DataService;
 import com.taskService.service.data.TaskService;
@@ -28,6 +33,9 @@ public class TaskController {
 	
 	@Autowired
 	DataService dataservice;
+	
+	@Autowired
+	RestTemplate restTemplate;
 
 	@RequestMapping(value = "/createTask", method = RequestMethod.POST)
 	public @ResponseBody JSONObject createTask(
@@ -47,6 +55,31 @@ public class TaskController {
 		}
 
 		return statusobj;
+	}
+	
+	@RequestMapping(value = "/postTask", method = RequestMethod.GET)
+	public @ResponseBody JSONObject notifyTask(
+			@RequestHeader(value = "auth_key") String auth_key,
+			@RequestParam(value = "taskTitle") String taskTitle)
+			throws URISyntaxException {
+		
+		 TaskModel taskmodel = taskservice.fetchTask(taskTitle);
+			JSONObject statusobj = new JSONObject();
+
+			if (taskmodel != null) {
+				String url = "http://localhost:8081/api/notifier/sendEmail";
+				
+				restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+				Gson gson = new Gson();
+				
+				String task = gson.toJson(taskmodel);
+				statusobj = restTemplate
+						.postForObject(url, task, JSONObject.class);
+				
+				return statusobj;
+				
+			}
+			return statusobj;
 	}
 
 	@RequestMapping(value = "/getUserTasks", method = RequestMethod.POST)
